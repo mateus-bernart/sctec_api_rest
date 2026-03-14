be174a016a383763716ea582a6b2b69ff5c79977
Mateus Bernart <mateusbernart14@gmail.com>
2026-03-14 14:02:49 -0300

    feat: adiciona InternalServerError error

--- infra/errors.js
+++ infra/errors.js
@@ -111,3 +111,23 @@ export class MethodNotAllowedError extends Error {
     };
   }
 }
+
+export class InternalServerError extends Error {
+  constructor({ cause, statusCode }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
